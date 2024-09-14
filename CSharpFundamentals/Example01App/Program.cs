02eea5e1fb35e6b3bfcb3ad2f0e1e9e8599eec57
Byron <byronalmeidaec@gmail.com>
2024-09-13 19:22:03 -0500

    New file created

---
+++ CSharpFundamentals/Example01App/Program.cs
@@ -0,0 +1,20 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaración de variables
+        int age = 25;
+        double pi = 3.1416;
+        string name = "Alice";
+        bool isStudent = true;
+
+        // Imprimir variables
+        Console.WriteLine("Nombre: " + name);
+        Console.WriteLine("Edad: " + age);
+        Console.WriteLine("Valor de Pi: " + pi);
+        Console.WriteLine("Es estudiante: " + isStudent);
+    }
+}
